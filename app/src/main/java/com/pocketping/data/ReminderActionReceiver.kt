cfabc25d5da37fbb63e934c186241095e4bf0824
priyans <309785143+Hotfix-Cde@users.noreply.github.com>
2026-08-08 08:12:23 +0000

    Add notification action receiver

---
+++ app/src/main/java/com/pocketping/data/ReminderActionReceiver.kt
@@ -0,0 +1,9 @@
+package com.pocketping.data
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ReminderActionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) = Unit
+}
