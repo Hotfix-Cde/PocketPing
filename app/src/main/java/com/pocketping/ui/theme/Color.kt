19fac30149360052baa4beba48cb6a5870023849
priyans <309785143+Hotfix-Cde@users.noreply.github.com>
2026-08-08 08:13:11 +0000

    Add PocketPing theme colors

---
+++ app/src/main/java/com/pocketping/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.pocketping.ui.theme
+
+import androidx.compose.ui.graphics.Color
+val Indigo80 = Color(0xFFC7D2FE)
+val Teal80 = Color(0xFF99F6E4)
+val Rose80 = Color(0xFFFBCFE8)
+val Indigo40 = Color(0xFF4338CA)
+val Teal40 = Color(0xFF0F766E)
+val Rose40 = Color(0xFFBE185D)
